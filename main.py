091cb1ff29d0704d336828eaa4bc80cb0b867729
EdineiAndrade <edinei22.ba@gmail.com>
2025-03-04 23:20:02 -0300

    add print finazando projeto

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     base_url = 'https://atacadodamodajeans.lojavirtualnuvem.com.br/produtos/?mpage=3'  
     data = scrape_modajeans(base_url)
     save_to_google_sheets(data,2)
-
+    print("Finalizado web scraping atacadodamodajeans")
 
     # base_url = 'https://triboshoes.com.br/'  
     # data = scrape_triboshoes(base_url)
