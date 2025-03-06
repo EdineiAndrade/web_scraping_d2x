1622d33b58a0b9910b2cb3266b49fb9bfa547d3c
EdineiAndrade <edinei22.ba@gmail.com>
2025-03-06 13:43:33 -0300

    ajuste preço

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
     # save_to_google_sheets(data, 3)
     # print("Finalizado web scraping atacadum")
 
-    base_url = 'https://atacadodamodajeans.lojavirtualnuvem.com.br/produtos/?mpage=3'  
-    data = scrape_modajeans(base_url)
-    save_to_google_sheets(data,2)
-    print("Finalizado web scraping atacadodamodajeans")
+    # base_url = 'https://atacadodamodajeans.lojavirtualnuvem.com.br/produtos/?mpage=3'  
+    # data = scrape_modajeans(base_url)
+    # save_to_google_sheets(data,2)
+    # print("Finalizado web scraping atacadodamodajeans")
 
     # base_url = 'https://triboshoes.com.br/'  
     # data = scrape_triboshoes(base_url)
