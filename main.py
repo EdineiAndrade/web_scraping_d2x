2d9bc51e1505f1e68c5a2a293bff03000908b4ca
EdineiAndrade <edinei22.ba@gmail.com>
2025-05-14 00:33:21 -0300

    adicionando scrape_07

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from scrape_03_modajeans import scrape_modajeans
 from scrape_04_atacadum import scrape_atacadum
 from scrape_05_florattajoias import scrape_florattajoias
 from scrape_06_cemstoretec import scrape_06_cemstoretec
+from scrape_07_c7_drop import scrape_c7_drop
 
 from sheets import save_to_google_sheets
 
@@ -14,10 +15,10 @@ if __name__ == "__main__":
     # data = scrape_gruposhopmix(base_url)
     # print("Finalizado web scraping gruposhopmix")
 
-    base_url = 'https://triboshoes.com.br/'
-    data = scrape_triboshoes(base_url)
-    save_to_google_sheets(data, 1)
-    print("Finalizado web scraping triboshoes")
+    # base_url = 'https://triboshoes.com.br/'
+    # data = scrape_triboshoes(base_url)
+    # save_to_google_sheets(data, 1)
+    # print("Finalizado web scraping triboshoes")
 
     # base_url = 'https://atacadodamodajeans.lojavirtualnuvem.com.br/produtos/?mpage=3'
     # data = scrape_modajeans(base_url)
@@ -37,4 +38,9 @@ if __name__ == "__main__":
     # base_url = 'https://cemstoretec.com.br/'
     # data = scrape_06_cemstoretec(base_url)
     # save_to_google_sheets(data, 5)
+    # print("Finalizado web scraping cemstoretec")
+
+    base_url = 'https://c7drop.com.br/'
+    data = scrape_c7_drop(base_url)
+    save_to_google_sheets(data, 6)
     print("Finalizado web scraping cemstoretec")
